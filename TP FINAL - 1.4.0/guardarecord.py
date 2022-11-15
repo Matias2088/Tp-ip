ce254a0efa6f3f0ad27ea9db0d9acfed1d0394b0
leonardo <leodjraziel@users.noreply.github.com>
2022-11-15 18:23:22 -0300

    Add files via upload

---
+++ TP FINAL - 1.4.0/guardarecord.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+import os, random, sys, math
+
+import pygame
+from pygame.locals import *
+from configuracion import *
+from extras import *
+from funcionesVACIAS import *
+from principal import *
+
