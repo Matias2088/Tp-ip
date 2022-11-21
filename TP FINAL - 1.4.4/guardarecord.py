40bccb14a10f41cff061a81e419c734a0ced1bf1
LuchoC00 <118071422+LuchoC00@users.noreply.github.com>
2022-11-21 15:48:35 -0300

    Add files via upload
    Se soluciono varios errores al guardar
    falta chequear el correcto funcionamiento de los puntos

---
+++ TP FINAL - 1.4.4/guardarecord.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+import os, random, sys, math
+
+import pygame
+from pygame.locals import *
+from configuracion import *
+from principal import *
+
+#crear funcion puntajes para guardar
+def guardar_puntajes(puntajes):
+    archivo = open("resultados.txt", "a")
+    for nombre, puntos, in puntajes:
+        archivo.write("\n"+nombre+","+str(puntos))
+    archivo.close()
+
+def recuperar_puntajes(nombre_archivo):
+    lista = []
+    archivo = open(nombre_archivo, "r")
+    for línea in archivo:
+        print(línea)
+        nombre,puntos = línea.rstrip("\\n").split(",")
+        lista.append((nombre,int(puntos)))
+    archivo.close()
+    return lista
+
+
+def ordenar(lista):
+    ordenados=sorted(lista)
+    return ordenados
+
+def mejores10(lista):
+    mejores=[]
+    x=0
+    for i in range(len(lista)):
+        if x<10:
+            mejores.append(lista[i])
+            x+=1
+    return mejores
+
+def devoluciones10():
+    nombre_archivo="resultados.txt"
+    lista=mejores10(ordenar(recuperar_puntajes(nombre_archivo)))
+    return lista
